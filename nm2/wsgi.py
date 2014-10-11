f40244321481f1af043633b1a71f3bdfa90b506c
Enrico Zini <enrico@enricozini.org>
2014-10-11 23:29:59 +0200

    Set umask and project root

--- nm2/wsgi.py
+++ nm2/wsgi.py
@@ -8,7 +8,16 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nm2.settings")
+os.umask(005)
+
+project_root = '/srv/nm.debian.org/nm2'
+paths = [project_root]
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
