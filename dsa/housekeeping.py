64767ad62c119ab78884d7f20738738e5ac0a295
Enrico Zini <enrico@enricozini.org>
2015-05-11 13:58:12 +0200

    Updated auto-import of changes from LDAP

--- dsa/housekeeping.py
+++ dsa/housekeeping.py
@@ -83,29 +83,64 @@ class NewGuestAccountsFromDSA(hk.Task):
             # Skip entries without emails (happens when running outside of the Debian network)
             if entry.single("emailForward") is None: continue
 
-            # Check for fingerprint duplicates
             try:
-                p = bmodels.Person.objects.get(fpr=entry.single("keyFingerPrint"))
-                self.hk.inconsistencies.log_person(self, p,
-                                                      "has the same fingerprint as LDAP uid {}".format(entry.uid),
-                                                      ldap_uid=entry.uid)
-                continue
+                fpr_person = bmodels.Person.objects.get(fpr=entry.single("keyFingerPrint"))
             except bmodels.Person.DoesNotExist:
-                pass
+                fpr_person = None
 
-            p = bmodels.Person.objects.create_user(
-                cn=entry.single("cn"),
-                mn=entry.single("mn") or "",
-                sn=entry.single("sn") or "",
-                email=entry.single("emailForward"),
-                uid=entry.uid,
-                fpr=entry.single("keyFingerPrint"),
-                status=const.STATUS_DC_GA,
-                username="{}@invalid.example.org".format(entry.uid),
-                audit_author=self.hk.housekeeper.user,
-                audit_notes="created new guest account entry from LDAP",
-            )
-            log.info("%s: %s (guest account only) imported from LDAP", self.IDENTIFIER, self.hk.link(p))
+            try:
+                email_person = bmodels.Person.objects.get(email=entry.single("emailForward"))
+            except bmodels.Person.DoesNotExist:
+                email_person = None
+
+            if fpr_person and email_person and fpr_person.pk != email_person.pk:
+                log.warn("%s: LDAP has new uid %s which corresponds to two different users in our db: %s (by fingerprint %s) and %s (by email %s)",
+                         self.IDENTIFIER, entry.uid,
+                         self.hk.link(fpr_person), entry.single("keyFingerPrint"),
+                         self.hk.link(email_person), entry.single("emailForward"))
+                continue
+
+            # Now we either have fpr_person or email_person or none of them, or
+            # both and they are the same: from now on we can work with only one
+            # person
+            person = fpr_person if fpr_person else email_person
+
+            if not person:
+                # New DC_GA
+                person = bmodels.Person.objects.create_user(
+                    cn=entry.single("cn"),
+                    mn=entry.single("mn") or "",
+                    sn=entry.single("sn") or "",
+                    email=entry.single("emailForward"),
+                    uid=entry.uid,
+                    fpr=entry.single("keyFingerPrint"),
+                    status=const.STATUS_DC_GA,
+                    username="{}@invalid.example.org".format(entry.uid),
+                    audit_author=self.hk.housekeeper.user,
+                    audit_notes="created new guest account entry from LDAP",
+                )
+                log.info("%s: %s (guest account only) imported from LDAP", self.IDENTIFIER, self.hk.link(person))
+            else:
+                if person.status in (const.STATUS_DC_GA, const.STATUS_DM_GA):
+                    # We already know about it: nothing to do
+                    pass
+                if person.status in (const.STATUS_DC, const.STATUS_DM):
+                    if person.status == const.STATUS_DM:
+                        # DM that becomes DM_GA (acquires uid)
+                        person.status = const.STATUS_DM_GA
+                    else:
+                        # DC that becomes DC_GA (acquires uid)
+                        person.status = const.STATUS_DC_GA
+                    person.uid = entry.uid
+                    audit_notes = "entry found in LDAP, adding 'guest account' status"
+                    person.save(audit_author=self.hk.housekeeper.user, audit_notes=audit_notes)
+                    log.info("%s: %s %s", self.IDENTIFIER, self.hk.link(person), audit_notes)
+                else:
+                    # New uid on a status that is not supposed to have one:
+                    # just warn about it
+                    log.warn("%s: LDAP has new uid %s for person %s, which already has status %s in our database",
+                             self.IDENTIFIER, entry.uid, self.hk.link(person), const.ALL_STATUS_DESCS[person.status])
+
 
 class CheckLDAPConsistency(hk.Task):
     """
