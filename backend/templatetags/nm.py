c237d8704d60e152d3e00f22075043fb89bb82ca
Enrico Zini <enrico@enricozini.org>
2015-08-29 16:39:34 +0200

    Source cleanup

--- backend/templatetags/nm.py
+++ backend/templatetags/nm.py
@@ -1,24 +1,11 @@
-# nm.debian.org website backend
-#
-# Copyright (C) 2012  Enrico Zini <enrico@debian.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
-from backend import const
+from .. import const
 
 register = template.Library()
 
