f7fbd3d27604c5d24f85abff460cdababa324e97
Enrico Zini <enrico@enricozini.org>
2016-05-31 18:39:47 +0200

    Fixed error in urls

--- process/urls.py
+++ process/urls.py
@@ -8,7 +8,7 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.List.as_view, name="process_list"),
+    url(r'^$', views.List.as_view(), name="process_list"),
     url(r'^create/(?P<key>[^/]+)$', views.Create.as_view(), name="process_create"),
     url(r'^(?P<pk>\d+)$', views.Show.as_view(), name="process_show"),
     url(r'^(?P<pk>\d+)/intent$', views.ReqIntent.as_view(), name="process_req_intent"),
