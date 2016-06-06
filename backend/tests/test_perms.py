146c30f53dadfe3bcc6de9e5880bf0e5162b56a2
Enrico Zini <enrico@enricozini.org>
2016-06-06 15:05:09 +0200

    Fixed backend perms tests after recent changes

--- backend/tests/test_perms.py
+++ backend/tests/test_perms.py
@@ -53,58 +53,70 @@ class TestVisitPersonNoProcess(OldProcessFixtureMixin, TestCase):
     @classmethod
     def __add_extra_tests__(cls):
         cls._add_method(cls._test_perms, "pending", perms=ExpectedPerms({
-            "fd dam": "update_keycheck edit_bio edit_ldap view_person_audit_log",
+            "fd dam": "update_keycheck edit_bio edit_ldap view_person_audit_log fd_comments",
+            "activeam": "update_keycheck edit_bio edit_ldap view_person_audit_log",
             "pending": "view_person_audit_log update_keycheck edit_bio",
             "dd_nu dd_u": "view_person_audit_log update_keycheck",
         }))
 
         cls._add_method(cls._test_perms, "dc", perms=ExpectedPerms({
-            "fd dam dc": "update_keycheck edit_bio edit_ldap view_person_audit_log request_new_status",
+            "fd dam": "update_keycheck edit_bio edit_ldap view_person_audit_log request_new_status fd_comments",
+            "dc": "update_keycheck edit_bio edit_ldap view_person_audit_log request_new_status",
+            "activeam": "update_keycheck edit_bio edit_ldap view_person_audit_log",
             "dd_nu dd_u": "view_person_audit_log update_keycheck",
-        }, {
-            "fd dam dd_nu dd_u": "dc_ga dm dd_u dd_nu",
-            "dm dm_ga": "dc_ga",
+        }, advs={
+            "fd dam dd_nu dd_u activeam": "dd_u dd_nu",
         }))
 
         cls._add_method(cls._test_perms, "dc_ga", perms=ExpectedPerms({
-            "fd dam dc_ga": "update_keycheck edit_bio view_person_audit_log request_new_status",
+            "fd dam": "update_keycheck edit_bio view_person_audit_log request_new_status fd_comments",
+            "dc_ga": "update_keycheck edit_bio view_person_audit_log request_new_status",
+            "activeam": "update_keycheck edit_bio view_person_audit_log",
             "dd_nu dd_u": "view_person_audit_log update_keycheck",
         }, {
-            "fd dam dd_nu dd_u": "dm_ga dd_u dd_nu",
+            "fd dam dd_nu dd_u activeam": "dd_u dd_nu",
         }))
 
         cls._add_method(cls._test_perms, "dm", perms=ExpectedPerms({
-            "fd dam dm": "update_keycheck edit_bio edit_ldap view_person_audit_log request_new_status",
+            "fd dam": "update_keycheck edit_bio edit_ldap view_person_audit_log request_new_status fd_comments",
+            "dm": "update_keycheck edit_bio edit_ldap view_person_audit_log request_new_status",
+            "activeam": "update_keycheck edit_bio edit_ldap view_person_audit_log",
             "dd_nu dd_u": "view_person_audit_log update_keycheck",
         }, advs={
-            "fd dam dd_nu dd_u": "dm_ga dd_u dd_nu",
-            "dm dm_ga": "dm_ga",
+            "fd dam dd_nu dd_u activeam": "dd_u dd_nu",
         }))
 
         cls._add_method(cls._test_perms, "dm_ga", perms=ExpectedPerms({
-            "fd dam dm_ga": "update_keycheck edit_bio view_person_audit_log request_new_status",
+            "fd dam": "update_keycheck edit_bio view_person_audit_log request_new_status fd_comments",
+            "dm_ga": "update_keycheck edit_bio view_person_audit_log request_new_status",
+            "activeam": "update_keycheck edit_bio view_person_audit_log",
             "dd_nu dd_u": "view_person_audit_log update_keycheck",
         }, advs={
-            "fd dam dd_nu dd_u": "dd_u dd_nu",
+            "fd dam dd_nu dd_u activeam": "dd_u dd_nu",
         }))
 
         cls._add_method(cls._test_perms, "dd_nu", perms=ExpectedPerms({
-            "fd dam dd_nu": "update_keycheck edit_bio view_person_audit_log request_new_status",
+            "fd dam": "update_keycheck edit_bio view_person_audit_log request_new_status fd_comments",
+            "dd_nu": "update_keycheck edit_bio view_person_audit_log request_new_status",
+            "activeam": "update_keycheck edit_bio view_person_audit_log",
             "dd_u": "view_person_audit_log update_keycheck",
         }))
 
         cls._add_method(cls._test_perms, "dd_u", perms=ExpectedPerms({
-            "fd dam dd_u": "update_keycheck edit_bio view_person_audit_log",
+            "fd dam": "update_keycheck edit_bio view_person_audit_log fd_comments",
+            "activeam dd_u": "update_keycheck edit_bio view_person_audit_log",
             "dd_nu": "view_person_audit_log update_keycheck",
         }))
 
         cls._add_method(cls._test_perms, "fd", perms=ExpectedPerms({
-            "fd dam": "update_keycheck edit_bio view_person_audit_log request_new_status",
+            "fd dam": "update_keycheck edit_bio view_person_audit_log request_new_status fd_comments",
+            "activeam": "update_keycheck edit_bio view_person_audit_log",
             "dd_nu dd_u": "view_person_audit_log update_keycheck",
         }))
 
         cls._add_method(cls._test_perms, "dam", perms=ExpectedPerms({
-            "fd dam": "update_keycheck edit_bio view_person_audit_log",
+            "fd dam": "update_keycheck edit_bio view_person_audit_log fd_comments",
+            "activeam": "view_person_audit_log update_keycheck edit_bio",
             "dd_nu dd_u": "view_person_audit_log update_keycheck",
         }))
 
