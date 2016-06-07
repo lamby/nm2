1dfe85f6bd947df29f86d7e8959ef5d63a7b7796
Enrico Zini <enrico@enricozini.org>
2016-06-07 22:18:09 +0200

    Added start process wizard and linked site wizard from big top buttons

--- wizard/urls.py
+++ wizard/urls.py
@@ -10,24 +10,5 @@ from . import views
 urlpatterns = [
     url(r'^$', VisitorTemplateView.as_view(template_name="wizard/home.html"), name="wizard_home"),
     url(r'^advocate$', views.Advocate.as_view(), name="wizard_advocate"),
-#    url(r'^newnm$', views.Newnm.as_view(), name="public_newnm"),
-#    url(r'^newnm/resend_challenge/(?P<key>[^/]+)$', views.NewnmResendChallenge.as_view(), name="public_newnm_resend_challenge"),
-#    url(r'^newnm/confirm/(?P<nonce>[^/]+)$', views.NewnmConfirm.as_view(), name="public_newnm_confirm"),
-#    url(r'^processes$', views.Processes.as_view(), name="processes"),
-#    url(r'^managers$', views.Managers.as_view(), name="managers"),
-#    url(r'^people(?:/(?P<status>\w+))?$', views.People.as_view(), name="people"),
-#    url(r'^person/(?P<key>[^/]+)$', views.Person.as_view(), name="person"),
-#    url(r'^process/(?P<key>[^/]+)$', views.Process.as_view(), name="public_process"),
-#    url(r'^process/(?P<key>[^/]+)/update_keycheck$', views.ProcessUpdateKeycheck.as_view(), name="public_process_update_keycheck"),
-#    url(r'^progress/(?P<progress>\w+)$', views.Progress.as_view(), name="public_progress"),
-#    url(r'^stats$', views.Stats.as_view(), name="public_stats"),
-#    url(r'^stats/latest$', views.StatsLatest.as_view(), name="public_stats_latest"),
-#    url(r'^stats/graph$', views.StatsGraph.as_view(), name="public_stats_graph"),
-#    url(r'^findperson$', views.Findperson.as_view(), name="public_findperson"),
-#    url(r'^audit_log$', views.AuditLog.as_view(), name="public_audit_log"),
-#
-#    # Compatibility
-#    url(r'^whoisam$', views.Managers.as_view(), name="public_whoisam"),
-#    url(r'^nmstatus/(?P<key>[^/]+)$', views.Process.as_view(), name="public_nmstatus"),
-#    url(r'^nmlist$', views.Processes.as_view(), name="public_nmlist"),
+    url(r'^process/(?P<applying_for>[^/]+)$', views.NewProcess.as_view(), name="wizard_newprocess"),
 ]
--- wizard/views.py
+++ wizard/views.py
@@ -7,6 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms, http
 from django.views.generic import TemplateView, FormView, View
 from backend.mixins import VisitorMixin
+from backend import const
 import backend.models as bmodels
 import process.models as pmodels
 from collections import defaultdict, OrderedDict
@@ -55,3 +56,61 @@ class Advocate(VisitorMixin, FormView):
 
     def form_valid(self, form):
         return self.render_to_response(self.get_context_data(form=form))
+
+
+class NewProcess(VisitorMixin, TemplateView):
+    template_name = "wizard/newprocess.html"
+
+    def get_context_data(self, **kw):
+        ctx = super(NewProcess, self).get_context_data(**kw)
+        target = self.kwargs["applying_for"]
+        comments = []
+        allowed = False
+
+        if target == "dm":
+            target_desc = "Become Debian Maintainer"
+        elif target == "ga":
+            target_desc = "Request guest account"
+        elif target == "return":
+            target_desc = "Return from Emeritus"
+        else:
+            target_desc = "Become {}".format(const.ALL_STATUS_DESCS[target])
+
+        if self.visitor:
+            whitelist = self.visitor.possible_new_statuses
+            if target == "dm":
+                if self.visitor.status in (const.STATUS_DM, const.STATUS_DM_GA):
+                    comments.append("You are already a Debian Maintainer: problem solved!")
+                elif self.visitor.status not in (const.STATUS_DC, const.STATUS_DC_GA):
+                    comments.append("You are already a {}.".format(const.ALL_STATUS_DESCS[self.visitor.status]))
+                else:
+                    allowed = const.STATUS_DM in whitelist or const.STATUS_DM_GA in whitelist
+                    if not allowed:
+                        comments.append("You cannot start a new process for Debian Maintainer. Did you already start one?")
+            elif target == "ga":
+                if self.visitor.status not in (const.STATUS_DC, const.STATUS_DM):
+                    comments.append("As a {}, you should already have access to porter machines.".format(const.ALL_STATUS_DESCS[self.visitor.status]))
+                else:
+                    allowed = const.STATUS_DC in whitelist or const.STATUS_DM in whitelist
+                    if not allowed:
+                        comments.append("You cannot request a guest account. Did you already request it?")
+            elif target == "return":
+                if self.visitor.status != const.STATUS_EMERITUS_DD:
+                    comments.append("You seem to be {}, not an Emeritus DD.".format(const.ALL_STATUS_DESCS[self.visitor.status]))
+                else:
+                    allowed = True
+            else:
+                if target == self.visitor.status:
+                    comments.append("You are already {}: problem solved!".format(
+                        const.ALL_STATUS_DESCS[target]))
+                else:
+                    allowed = target in whitelist
+                    if not allowed:
+                        comments.append("You are currently {} and you cannot become {}.".format(
+                            const.ALL_STATUS_DESCS[self.visitor.status],
+                            const.ALL_STATUS_DESCS[target]))
+
+        ctx["comments"] = comments
+        ctx["allowed"] = allowed
+        ctx["target_desc"] = target_desc
+        return ctx
