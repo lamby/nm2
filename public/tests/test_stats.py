798d6b9d78a5dafb2a5abf810b6aeec28ae29579
Enrico Zini <enrico@enricozini.org>
2016-06-13 08:29:11 +0200

    Reproduced a server error

---
+++ public/tests/test_stats.py
@@ -0,0 +1,27 @@
+# coding: utf8
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.utils.timezone import now
+from backend import const
+from backend import models as bmodels
+from backend.unittest import PersonFixtureMixin, ExpectedSets, TestSet, PageElements
+import process.models as pmodels
+from mock import patch
+import tempfile
+import mailbox
+
+class TestStats(PersonFixtureMixin, TestCase):
+    def test_get(self):
+        client = self.make_test_client(None)
+        response = client.get(reverse("public_stats"))
+        self.assertEqual(response.status_code, 200)
+        response = client.get(reverse("public_stats"), data={"json": True, "days": 30})
+        self.assertEqual(response.status_code, 200)
+        response = client.get(reverse("public_stats_latest"), data={"json": True, "days": 30})
+        self.assertEqual(response.status_code, 200)
+        response = client.get(reverse("public_stats_graph"), data={"json": True, "days": 30})
+        self.assertEqual(response.status_code, 200)
