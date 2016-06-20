650baff3a39fb021aada61826c56e1dcb342eda7
Enrico Zini <enrico@enricozini.org>
2016-06-20 02:21:59 +0200

    More aware datetime fixes

--- public/templatetags/none_if_epoch.py
+++ public/templatetags/none_if_epoch.py
@@ -1,11 +1,12 @@
 from django import template
+from django.utils.timezone import utc
 import datetime
 
 register = template.Library()
 
 @register.filter
 def none_if_epoch(value):
-    epoch = datetime.datetime(1970, 1, 1)    
+    epoch = datetime.datetime(1970, 1, 1, tzinfo=utc)
     if value == epoch:
         return None
     else:
