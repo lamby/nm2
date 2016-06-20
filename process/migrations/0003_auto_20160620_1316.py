acb731f2c8930336f284705093b394f75fc00cf8
Enrico Zini <enrico@enricozini.org>
2016-06-20 15:18:46 +0200

    added migration

---
+++ process/migrations/0003_auto_20160620_1316.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('process', '0002_auto_20160531_1920'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='process',
+            name='approved_time',
+            field=models.DateTimeField(help_text='Date the process was reviewed and considered complete, or NULL if not yet reviewed', null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='process',
+            name='closed',
+            field=models.DateTimeField(help_text='Date the process was closed, or NULL if still open', null=True, blank=True),
+        ),
+    ]
