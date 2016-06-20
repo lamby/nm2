3213db08bea5b488cc8e056043901dabf425c2a2
Enrico Zini <enrico@enricozini.org>
2016-06-20 11:51:22 +0200

    Fixed after testing on nono

--- keyring/housekeeping.py
+++ keyring/housekeeping.py
@@ -329,5 +329,5 @@ class CheckKeyringLogs(hk.Task):
                 op.execute()
 
             # Update our bookmark
-            gk.git.update_ref("refs/heads/keyring_maint_import", state["commit"])
+            gk.git.update_ref("refs/heads/keyring_maint_import", entry.shasum)
             log.info("%s: Updating ref keyring_maint_import to commit %s", self.IDENTIFIER, state["commit"])
