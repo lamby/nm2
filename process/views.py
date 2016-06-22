319cecf1d96aa1866ba3f2da39f627e67c67e01b
Enrico Zini <enrico@enricozini.org>
2016-06-22 15:33:55 +0200

    Do not have two intros for guest account requests

--- process/views.py
+++ process/views.py
@@ -497,7 +497,8 @@ class MakeRTTicket(VisitProcessMixin, TemplateView):
 
 
         # Build the request text
-        request.append("Please make {person.fullname} (currently '{status}') a '{applying_for}' (advocated by {sponsors}).")
+        if not only_guest_account:
+            request.append("Please make {person.fullname} (currently '{status}') a '{applying_for}' (advocated by {sponsors}).")
 
         if not only_guest_account:
             if self.person.status == const.STATUS_DC:
