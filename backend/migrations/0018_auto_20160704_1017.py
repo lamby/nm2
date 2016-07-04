1868416a238a1942c6740257af41c97fa4756f4d
Enrico Zini <enrico@enricozini.org>
2016-07-04 12:20:06 +0200

    Added data migration to fill email_ldap from email

---
+++ backend/migrations/0018_auto_20160704_1017.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+def fill_email_ldap(apps, schema_editor):
+    Person = apps.get_model("backend", "Person")
+    #robot = Person.objects.get(username="__housekeeping__")
+    for person in Person.objects.all():
+        person.email_ldap = person.email
+        person.save()
+        #person.save(audit_notes="Initialized email_ldap from existing email field", audit_author=robot)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('backend', '0017_person_email_ldap'),
+    ]
+
+    operations = [
+        migrations.RunPython(fill_email_ldap),
+    ]
