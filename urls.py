df99bb9dad01d2c76ea73445759b17aba4ba9a0f
Enrico Zini <enrico@enricozini.org>
2017-02-08 13:59:52 +0100

    Add includes for debug toolbar when DEBUG is True

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from backend.mixins import VisitorTemplateView
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -34,3 +35,12 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+        urlpatterns += [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ]
+    except ImportError:
+        pass
