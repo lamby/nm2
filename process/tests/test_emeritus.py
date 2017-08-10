c60a889fcbd924fc4912423ee3df642fee48ab57
Enrico Zini <enrico@enricozini.org>
2017-08-10 13:24:12 -0400

    Also test the process and requirements created by post

--- process/tests/test_emeritus.py
+++ process/tests/test_emeritus.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from django.core import mail
 from backend.unittest import PersonFixtureMixin
+from backend import const
+import process.models as pmodels
 import process.views as pviews
 from .common import (ProcessFixtureMixin,
                      test_fingerprint1, test_fpr1_signed_valid_text,
@@ -26,34 +28,45 @@ class TestEmeritus(ProcessFixtureMixin, TestCase):
     def _test_success(self, visitor):
         mail.outbox = []
         client = self.make_test_client(visitor)
-        self._test_success_common(client, reverse("process_emeritus"))
+        self._test_success_common(visitor, client, reverse("process_emeritus"))
 
     def _test_nonsso_success(self, visitor):
         mail.outbox = []
         url = pviews.Emeritus.get_nonauth_url(self.persons[visitor])
         client = self.make_test_client(None)
-        self._test_success_common(client, url)
+        self._test_success_common(visitor, client, url)
 
-    def _test_success_common(self, client, url):
+    def _test_success_common(self, visitor, client, url):
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
         response = client.post(url, data={"statement": "test statement"})
         self.assertRedirectMatches(response, r"/process/\d+$")
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].to, ["debian-private@lists.debian.org"])
+        visitor = self.persons[visitor]
+        process = pmodels.Process.objects.get(person=visitor, applying_for=const.STATUS_EMERITUS_DD, closed__isnull=True)
+        req = process.requirements.get(type="intent")
+        status = req.compute_status()
+        self.assertTrue(status["satisfied"])
+        self.assertEqual(req.statements.count(), 1)
+        stm = req.statements.all()[0]
+        self.assertEqual(stm.requirement, req)
+        self.assertIsNone(stm.fpr)
+        self.assertEqual(stm.statement, "test statement")
+        self.assertEqual(stm.uploaded_by, visitor)
 
     def _test_forbidden(self, visitor):
         mail.outbox = []
         client = self.make_test_client(visitor)
-        self._test_forbidden_common(client, reverse("process_emeritus"))
+        self._test_forbidden_common(visitor, client, reverse("process_emeritus"))
 
     def _test_nonsso_forbidden(self, visitor):
         mail.outbox = []
         url = pviews.Emeritus.get_nonauth_url(self.persons[visitor])
         client = self.make_test_client(None)
-        self._test_forbidden_common(client, url)
+        self._test_forbidden_common(visitor, client, url)
 
-    def _test_forbidden_common(self, client, url):
+    def _test_forbidden_common(self, visitor, client, url):
         response = client.get(url)
         self.assertPermissionDenied(response)
         response = client.post(url, data={"statement": "test statement"})
